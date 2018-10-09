f875060597f34b21db0c65996bdef5ea6316de7a
buinak <fforeseer@gmail.com>
2018-10-09 02:39:50 +0200

    Set up data persistence and fetching.

--- app/src/main/java/com/buinak/curreq/UI/LoadingScreen/LoadingActivity.java
+++ app/src/main/java/com/buinak/curreq/UI/LoadingScreen/LoadingActivity.java
@@ -5,17 +5,9 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 import com.buinak.curreq.Data.DataSource;
-import com.buinak.curreq.Data.Local.CurrencyDatabase;
-import com.buinak.curreq.Data.Local.LocalDataSource;
-import com.buinak.curreq.Data.Remote.RemoteDataSource;
-import com.buinak.curreq.Data.Remote.CurrencyRepository;
 import com.buinak.curreq.Data.Repository;
 import com.buinak.curreq.Entities.CurreqEntity.RateRequestRecord;
 import com.buinak.curreq.R;
-
-import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.disposables.Disposable;
-import io.reactivex.schedulers.Schedulers;
 
 
 public class LoadingActivity extends AppCompatActivity implements DataSource.DataSourceListener {
@@ -26,7 +18,7 @@ public class LoadingActivity extends AppCompatActivity implements DataSource.Dat
         setContentView(R.layout.activity_loading);
 
         DataSource source = Repository.getNewInstance(this);
-        source.requestNewRecord();
+        source.requestRecord();
     }
 
     @Override
