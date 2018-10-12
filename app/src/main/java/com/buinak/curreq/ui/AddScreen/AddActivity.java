d046f275550d60b5ee8f41c852f76bebb6d84062
buinak <fforeseer@gmail.com>
2018-10-13 00:53:48 +0200

    Added a new screen

---
+++ app/src/main/java/com/buinak/curreq/ui/AddScreen/AddActivity.java
@@ -0,0 +1,19 @@
+package com.buinak.curreq.ui.AddScreen;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+
+import com.buinak.curreq.R;
+
+import butterknife.BindView;
+
+public class AddActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add);
+
+    }
+}
--- app/src/main/java/com/buinak/curreq/ui/MainScreen/MainActivity.java
+++ app/src/main/java/com/buinak/curreq/ui/MainScreen/MainActivity.java
@@ -1,15 +1,31 @@
 package com.buinak.curreq.ui.MainScreen;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 
 import com.buinak.curreq.R;
+import com.buinak.curreq.ui.AddScreen.AddActivity;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.floatingActionButton)
+    FloatingActionButton floatingActionButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+
+        floatingActionButton.setOnClickListener(v -> {
+            Intent intent = new Intent(this, AddActivity.class);
+            startActivity(intent);
+        });
     }
 }
